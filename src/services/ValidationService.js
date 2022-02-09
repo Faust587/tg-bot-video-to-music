42618f7bb429d8c7d561e4608fc69ac92c87f0e0
Yura Synkulych <kri3g0vec@gmail.com>
2022-02-09 09:54:47 +0200

    create regex for user links

--- src/services/ValidationService.js
+++ src/services/ValidationService.js
@@ -20,6 +20,26 @@ const validateUserVideoLink = videoLink => {
   return validationResult;
 };
 
+const validateUserLink = url => {
+  const result = {
+    ok: true,
+    type: null,
+    url: url
+  }
+  const YouTubeRegexp = /https:\/\/www.youtube.com\/watch?/;
+  const TikTokRegexp = /https:\/\/www.tiktok.com\//;
+  if (url.match(YouTubeRegexp)) {
+    result.type = 'youtube';
+  } else if (url.match(TikTokRegexp)) {
+    result.type = 'tiktok';
+  } else {
+    result.ok = false;
+  }
+
+  return result;
+}
+
 module.exports = {
   validateUserVideoLink,
-}
+  validateUserLink,
+};
